af735fc10f611d0b1c03acd4d228f77182b445a4
Andre <elij.richards22@gmail.com>
2016-05-10 04:25:11 -0400

    update commit

---
+++ js/questionFiller.js
@@ -0,0 +1,59 @@
+var question1 = {
+  title: "Germany"
+  flag: "images/flag/germFlag.png"
+  pic: "images/silo/germany.png"
+  rightAnswers: ["The top of the head is considered sacred here. So unless you are a chief, you should leave your fashionable hats and sunglasses home. A 16th century missionary was eaten for this big no-no.",
+                  "Strangely enough, it is possible to be in two different time zones at the same time when on the island of Taveuni due to the fact that Fiji is spread across the international dateline."],
+  wrongAnswers: ["Though FIJI Water bears its namesake, the true origin of this popular refreshment lies in the US. The upscale drink however does provide a percentage of total sales to the back to Fiji.",
+                  "Fiji is an archipelago formed through volcanic activity made up of 333 islands. The population of more than 850,000 occupy more than half of these islands."],
+  addlInfo: ["FIJI Water is a brand of bottled water derived, bottled, and shipped from a Fijian artesian aquifer in Viti Levu.",
+              "Of the total 333 islands, only 110 of them are populated with citizens."]
+};
+
+var question2 = {
+  title: "Madagascar"
+  flag: "images/flag/madaFlag.png"
+  pic: "images/silo/madagascar.png"
+  rightAnswers: ["The top of the head is considered sacred here. So unless you are a chief, you should leave your fashionable hats and sunglasses home. A 16th century missionary was eaten for this big no-no.",
+                  "Strangely enough, it is possible to be in two different time zones at the same time when on the island of Taveuni due to the fact that Fiji is spread across the international dateline."],
+  wrongAnswers: ["Though FIJI Water bears its namesake, the true origin of this popular refreshment lies in the US. The upscale drink however does provide a percentage of total sales to the back to Fiji.",
+                  "Fiji is an archipelago formed through volcanic activity made up of 333 islands. The population of more than 850,000 occupy more than half of these islands."],
+  addlInfo: ["FIJI Water is a brand of bottled water derived, bottled, and shipped from a Fijian artesian aquifer in Viti Levu.",
+              "Of the total 333 islands, only 110 of them are populated with citizens."]
+};
+
+var question3 = {
+  title: "Trinidad and Tobago"
+  flag: "images/flag/trinFlag.png"
+  pic: "images/silo/trinidad.png"
+  rightAnswers: ["The top of the head is considered sacred here. So unless you are a chief, you should leave your fashionable hats and sunglasses home. A 16th century missionary was eaten for this big no-no.",
+                  "Strangely enough, it is possible to be in two different time zones at the same time when on the island of Taveuni due to the fact that Fiji is spread across the international dateline."],
+  wrongAnswers: ["Though FIJI Water bears its namesake, the true origin of this popular refreshment lies in the US. The upscale drink however does provide a percentage of total sales to the back to Fiji.",
+                  "Fiji is an archipelago formed through volcanic activity made up of 333 islands. The population of more than 850,000 occupy more than half of these islands."],
+  addlInfo: ["FIJI Water is a brand of bottled water derived, bottled, and shipped from a Fijian artesian aquifer in Viti Levu.",
+              "Of the total 333 islands, only 110 of them are populated with citizens."]
+};
+
+var question4 = {
+  title: "China"
+  flag: "images/flag/chinFlag.png"
+  pic: "images/silo/china.png"
+  rightAnswers: ["The top of the head is considered sacred here. So unless you are a chief, you should leave your fashionable hats and sunglasses home. A 16th century missionary was eaten for this big no-no.",
+                  "Strangely enough, it is possible to be in two different time zones at the same time when on the island of Taveuni due to the fact that Fiji is spread across the international dateline."],
+  wrongAnswers: ["Though FIJI Water bears its namesake, the true origin of this popular refreshment lies in the US. The upscale drink however does provide a percentage of total sales to the back to Fiji.",
+                  "Fiji is an archipelago formed through volcanic activity made up of 333 islands. The population of more than 850,000 occupy more than half of these islands."],
+  addlInfo: ["FIJI Water is a brand of bottled water derived, bottled, and shipped from a Fijian artesian aquifer in Viti Levu.",
+              "Of the total 333 islands, only 110 of them are populated with citizens."]
+};
+
+var question5 = {
+  title: "FIji"
+  flag: "images/flag/fijiFlag.png"
+  pic: "images/silo/fiji.png"
+  rightAnswers: ["The top of the head is considered sacred here. So unless you are a chief, you should leave your fashionable hats and sunglasses home. A 16th century missionary was eaten for this big no-no.",
+                  "Strangely enough, it is possible to be in two different time zones at the same time when on the island of Taveuni due to the fact that Fiji is spread across the international dateline."],
+  wrongAnswers: ["Though FIJI Water bears its namesake, the true origin of this popular refreshment lies in the US. The upscale drink however does provide a percentage of total sales to the back to Fiji.",
+                  "Fiji is an archipelago formed through volcanic activity made up of 333 islands. The population of more than 850,000 occupy more than half of these islands."],
+  addlInfo: ["FIJI Water is a brand of bottled water derived, bottled, and shipped from a Fijian artesian aquifer in Viti Levu.",
+              "Of the total 333 islands, only 110 of them are populated with citizens."]
+};
