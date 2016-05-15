8bd3b94cf031e90bc1ec3349676f2874ad706c86
Andre <elij.richards22@gmail.com>
2016-05-15 06:51:06 -0400

    to be continued commit

--- js/actions.js
+++ js/actions.js
@@ -1,5 +1,6 @@
 'use strict'
-var score = document.getElementById('score');
+var scoreText = document.getElementById('scoreText');
+var scoreNumber = document.getElementById('scoreNumber');
 var answerFeedback = document.getElementById('answerFeedback');
 var factBlock = document.getElementsByClassName('factBlock');
 var flag = document.getElementsByClassName('flag');
@@ -10,9 +11,16 @@ var questionTwo = document.getElementsByClassName('questionTwo');
 var questionThree = document.getElementsByClassName('questionThree');
 var questionFour = document.getElementsByClassName('questionFour');
 var questionFive = document.getElementsByClassName('questionFive');
+var marker = document.getElementsByClassName('marker');
+var markerOne = document.getElementsByClassName('markerOne');
+var markerTwo = document.getElementsByClassName('markerTwo');
+var markerThree = document.getElementsByClassName('markerThree');
+var markerFour = document.getElementsByClassName('markerFour');
+var markerFive = document.getElementsByClassName('markerFive');
 var playBtn = document.getElementsByClassName('playBtn');
 var submitBtn = document.getElementsByClassName('submitBtn');
 var nextBtn = document.getElementsByClassName('nextBtn');
+var infoBox = document.getElementsByClassName('infoBox');
 var moreInfo = document.getElementsByClassName('moreInfo');
 var countryImg = document.getElementsByClassName('countryImg');
 var factAlpha = document.getElementsByClassName('factAlpha');
@@ -22,10 +30,16 @@ var factDelta = document.getElementsByClassName('factDelta');
 
 
 var count=0;
+var aCount=0;
+var bCount=0;
+var cCount=0;
+var dCount=0;
 var aClick=false;
 var bClick=false;
 var cClick=false;
 var dClick=false;
+var questionLoadCount = 1;
+var scoreCount = 0;
 
 
 function hideSections() {
@@ -37,61 +51,68 @@ function hideSections() {
 }
 
 function loadQuestionOne() {
-  $(questionOne).show();
-  $(flag).append('<img src='+(question1.flag)+' />')
+  $(questionOne).show().text('1');
+  $(flag).html('<img src='+(question1.flag)+' />')
   $(country_title).text(question1.title);
-  $(countryImg).append('<img src='+(question1.pic)+' />');
-  $(factAlpha).append(question1.rightAnswers[0]);
-  $(factBeta).append(question1.rightAnswers[1]);
-  $(factCharlie).append(question1.wrongAnswers[0]);
-  $(factDelta).append(question1.wrongAnswers[1]);
-  $(moreInfo).append('<li>'+question1.addlInfo[0]+'</li>');
-  $(moreInfo).append('<li></li>');
-  $(moreInfo).append('<li>'+question1.addlInfo[1]+'</li>');
+  $(countryImg).html('<img src='+(question1.pic)+' />');
+  $(factAlpha).html(question1.rightAnswers[0]);
+  $(factBeta).html(question1.rightAnswers[1]);
+  $(factCharlie).html(question1.wrongAnswers[0]);
+  $(factDelta).html(question1.wrongAnswers[1]);
+  $(moreInfo).html('<li>'+question1.addlInfo[0]+'</li>');
+  $(moreInfo).html('<li>'+question1.addlInfo[1]+'</li>');
 }
 
 function loadQuestionTwo() {
-  $(questionTwo).show();
-  $(flag).append('<img src='+(question2.flag)+' />')
+  $(questionTwo).show().text('2');
+  $(flag).html('<img src='+(question2.flag)+' />')
   $(country_title).text(question2.title);
-  $(countryImg).append('<img src='+(question2.pic)+' />');
-  $(factAlpha).append(question2.rightAnswers[0]);
-  $(factBeta).append(question2.rightAnswers[1]);
-  $(factCharlie).append(question2.wrongAnswers[0]);
-  $(factDelta).append(question2.wrongAnswers[1]);
+  $(countryImg).html('<img src='+(question2.pic)+' />');
+  $(factAlpha).html(question2.rightAnswers[0]);
+  $(factBeta).html(question2.rightAnswers[1]);
+  $(factCharlie).html(question2.wrongAnswers[0]);
+  $(factDelta).html(question2.wrongAnswers[1]);
+  $(moreInfo).html('<li>'+question2.addlInfo[0]+'</li>');
+  $(moreInfo).html('<li>'+question2.addlInfo[1]+'</li>');
 }
 
 function loadQuestionThree() {
-  $(questionThree).show();
-  $(flag).append('<img src='+(question3.flag)+' />')
+  $(questionThree).show().text('3');
+  $(flag).html('<img src='+(question3.flag)+' />')
   $(country_title).text(question3.title);
-  $(countryImg).append('<img src='+(question3.pic)+' />');
-  $(factAlpha).append(question3.rightAnswers[0]);
-  $(factBeta).append(question3.rightAnswers[1]);
-  $(factCharlie).append(question3.wrongAnswers[0]);
-  $(factDelta).append(question3.wrongAnswers[1]);
+  $(countryImg).html('<img src='+(question3.pic)+' />');
+  $(factAlpha).html(question3.rightAnswers[0]);
+  $(factBeta).html(question3.rightAnswers[1]);
+  $(factCharlie).html(question3.wrongAnswers[0]);
+  $(factDelta).html(question3.wrongAnswers[1]);
+  $(moreInfo).html('<li>'+question3.addlInfo[0]+'</li>');
+  $(moreInfo).html('<li>'+question3.addlInfo[1]+'</li>');
 }
 
 function loadQuestionFour() {
-  $(questionFour).show();
-  $(flag).append('<img src='+(question4.flag)+' />')
+  $(questionFour).show().text('4');
+  $(flag).html('<img src='+(question4.flag)+' />')
   $(country_title).text(question4.title);
-  $(countryImg).append('<img src='+(question4.pic)+' />');
-  $(factAlpha).append(question4.rightAnswers[0]);
-  $(factBeta).append(question4.rightAnswers[1]);
-  $(factCharlie).append(question4.wrongAnswers[0]);
-  $(factDelta).append(question4.wrongAnswers[1]);
+  $(countryImg).html('<img src='+(question4.pic)+' />');
+  $(factAlpha).html(question4.rightAnswers[0]);
+  $(factBeta).html(question4.rightAnswers[1]);
+  $(factCharlie).html(question4.wrongAnswers[0]);
+  $(factDelta).html(question4.wrongAnswers[1]);
+  $(moreInfo).html('<li>'+question4.addlInfo[0]+'</li>');
+  $(moreInfo).html('<li>'+question4.addlInfo[1]+'</li>');
 }
 
 function loadQuestionFive() {
-  $(questionFive).show();
-  $(flag).append('<img src='+(question5.flag)+' />')
+  $(questionFive).show().text('5');
+  $(flag).html('<img src='+(question5.flag)+' />')
   $(country_title).text(question5.title);
-  $(countryImg).append('<img src='+(question5.pic)+' />');
-  $(factAlpha).append(question5.rightAnswers[0]);
-  $(factBeta).append(question5.rightAnswers[1]);
-  $(factCharlie).append(question5.wrongAnswers[0]);
-  $(factDelta).append(question5.wrongAnswers[1]);
+  $(countryImg).html('<img src='+(question5.pic)+' />');
+  $(factAlpha).html(question5.rightAnswers[0]);
+  $(factBeta).html(question5.rightAnswers[1]);
+  $(factCharlie).html(question5.wrongAnswers[0]);
+  $(factDelta).html(question5.wrongAnswers[1]);
+  $(moreInfo).html('<li>'+question5.addlInfo[0]+'</li>');
+  $(moreInfo).html('<li>'+question5.addlInfo[1]+'</li>');
 }
 
 function resetClicks() {
@@ -100,15 +121,54 @@ function resetClicks() {
   cClick=false;
   dClick=false;
   count=0;
+  $(factBlock).removeClass('factSelection');
+}
+
+function duplicateClick() {
+  if (aCount > 1){
+    $(factAlpha).removeClass('factSelection');
+    aCount=0;
+    aClick=false;
+    count=1;
+  }
+
+  if (bCount > 1){
+    $(factBeta).removeClass('factSelection');
+    bCount=0;
+    bClick=false;
+    count=1;
+  }
+
+  if (cCount > 1){
+    $(factCharlie).removeClass('factSelection');
+    cCount=0;
+    cClick=false;
+    count=1;
+  }
+
+  if (dCount > 1){
+    $(factDelta).removeClass('factSelection');
+    dCount=0;
+    dClick=false;
+    count=1;
+  }
 }
 
 function factBlockClick() {
   count++;
-  if (count > 2){
+  if (count > 3){
     alert('Please select only TWO answers');
     resetClicks();
-    $(factBlock).removeClass('factSelection');
   }
+}
+
+function nextQuestionSetup() {
+  resetClicks();
+  $(nextBtn).hide();
+  $(submitBtn).show();
+  $(country_title).text('');
+  $(answerFeedback).text('???');
+  $(moreInfo).hide();
 }
 
 $(document).ready(function() {
@@ -122,56 +182,107 @@ $(document).ready(function() {
     loadQuestionOne();
   });
 
-  $(factAlpha).click(function (event) {
+  $(factAlpha).click(function () {
     aClick=true;
+    aCount++;
     $(this).addClass('factSelection');
     factBlockClick();
+    duplicateClick();
   });
 
-  $(factBeta).click(function (event) {
+  $(factBeta).click(function () {
     bClick=true;
+    bCount++;
     $(this).addClass('factSelection');
     factBlockClick();
+    duplicateClick();
   });
 
-  $(factCharlie).click(function (event) {
+  $(factCharlie).click(function () {
     cClick=true;
+    cCount++;
     $(this).addClass('factSelection');
     factBlockClick();
+    duplicateClick();
   });
 
-  $(factDelta).click(function (event) {
+  $(factDelta).click(function () {
     dClick=true;
+    dCount++;
     $(this).addClass('factSelection');
     factBlockClick();
+    duplicateClick();
   });
 
   $('.submitBtn').click(function () {
     if (count == 2){
       if (aClick == true && bClick == true){
-          $(answerFeedback).text('You\'re Right!');
-         resetClicks();
+         $(answerFeedback).text('You\'re Right!');
+         $(markerOne).append('<img src="images/icon/check.png">');
          $(moreInfo).show();
          $(submitBtn).hide();
          $(nextBtn).show();
-         $(factBlock).removeClass('factSelection');
-       } else if(cClick == true || dClick == true) {
+         questionLoadCount++;
+         scoreCount+=20;
+       } else {
            $(answerFeedback).text('Sorry!');
-           resetClicks();
+           $(markerOne).append('<img src="images/icon/ex.png">');
            $(moreInfo).show();
            $(submitBtn).hide();
            $(nextBtn).show();
-           $(factBlock).removeClass('factSelection');
+           questionLoadCount++;
+           scoreCount-=20;
          }
+         if (scoreCount == 100){
+           scoreNumber.style.color = '#1ADB61';
+         } else if (scoreCount >= 0){
+            scoreNumber.style.color = '#fff';
+            } else{
+                scoreNumber.style.color = '#DB1A1A';
+         }
+         $(scoreNumber).text(scoreCount);
     } else if (count == 0){
         alert('Please make your first selection');
       } else if (count <2){
-            alert('Please make a second selection');
-          }
+          alert('Please make a second selection');
+        }
+  });
+
+  $('.nextBtn').click(function () {
+    nextQuestionSetup();
+    switch (questionLoadCount) {
+      case 2:
+        loadQuestionTwo();
+        $(questionTwo).show();
+      break;
+      case 3:
+        loadQuestionThree();
+        $(questionThree).show();
+      break;
+      case 4:
+        loadQuestionFour();
+        $(questionFour).show();
+      break;
+      case 5:
+        loadQuestionFive();
+        $(questionFive).show();
+        //  will show results
+      break;
+      case 6:
+        $(submitBtn).hide();
+        $(playBtn).show();
+        $(questionMarker).hide();
+        resetClicks();
+        scoreCount = 0;
+        $(scoreCount).text('0');
+
+
+    }
+
   });
 
   $('.legend_icon').click(function () {
-
+    $(infoBox).append('<p>Test your knowledge of the world by choosing from the four answers below. Which TWO facts about the shown country are not facts at all? Choose wisely and Win!</p>');
   });
 
 });
--- js/questionFiller.js
+++ js/questionFiller.js
@@ -2,19 +2,19 @@ var question1 = {
   title: "Germany",
   flag: "images/flag/germFlag.png",
   pic: "images/silo/germany.png",
-  rightAnswers: ["The top of the head is considered sacred here. So unless you are a chief, you should leave your fashionable hats and sunglasses home. A 16th century missionary was eaten for this big no-no.",
-                  "Strangely enough, it is possible to be in two different time zones at the same time when on the island of Taveuni due to the fact that Fiji is spread across the international dateline."],
-  wrongAnswers: ["Although FIJI Water bears its namesake, the true origin of this popular refreshment lies in the US. The upscale drink however does provide a percentage of total sales back to the Fijian government.",
-                  "Fiji is an archipelago made up of 333 islands that were formed through volcanic activity. The population of more than 850,000 citizens occupy more than half of these islands."],
-  addlInfo: ["- FIJI Water is a brand of bottled water derived, bottled, and shipped from a Fijian artesian aquifer in Viti Levu.",
-              "- Of the total 333 islands, only 110 of them are populated with citizens."]
+  rightAnswers: ["The city of Frankfurt am Main's urban area boasts the highest per capita number of millionaires than any other city worldwide. Ranking second on the list, is the southern city of Munich.",
+                  "Over the course of this country's history, it has had a total of five capital cities: Aachen, Regensburg, Frankfurt am Main, Nuremberg, and now Berlin. Six if you count West Germany's Bonn."],
+  wrongAnswers: ["Basing the modern car design concept upon early American models such as Henry Ford's Model-T, German auto manufacturers such as BMW are known as the best-selling luxury brand vehicles in the world.",
+                  "The Oktoberfest held since 1810 in Munich is the largest Volksfest in the world with over 6 million visitors annually. Its named is derived from the month in which the festivities begin - October."],
+  addlInfo: ["- The modern car, the Motorwagen, was actually originated in the city of Mannheim by Karl Benz in 1885.",
+              "- Despite the name, the Oktoberfest starts at the end of September, lasting until the first weekend in October."]
 };
 
 var question2 = {
   title: "Madagascar",
   flag: "images/flag/madaFlag.png",
   pic: "images/silo/madagascar.png",
-  rightAnswers: ["The top of the head is considered sacred here. So unless you are a chief, you should leave your fashionable hats and sunglasses home. A 16th century missionary was eaten for this big no-no.",
+  rightAnswers: ["222222The top of the head is considered sacred here. So unless you are a chief, you should leave your fashionable hats and sunglasses home. A 16th century missionary was eaten for this big no-no.",
                   "Strangely enough, it is possible to be in two different time zones at the same time when on the island of Taveuni due to the fact that Fiji is spread across the international dateline."],
   wrongAnswers: ["Although FIJI Water bears its namesake, the true origin of this popular refreshment lies in the US. The upscale drink however does provide a percentage of total sales back to the Fijian government.",
                   "Fiji is an archipelago made up of 333 islands that were formed through volcanic activity. The population of more than 850,000 citizens occupy more than half of these islands."],
